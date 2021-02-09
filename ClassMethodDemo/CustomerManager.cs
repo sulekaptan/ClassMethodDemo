1af47c6e793778f1966babb72168bdb4054ddd59
sulekaptan <sule.kaptan@bil.omu.edu.tr>
2021-02-09 22:28:40 +0300

    add all files

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMethodDemo
+{
+    class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Congratulations! Customer added: " + customer.Name + " " + customer.Surname);
+        }
+
+
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine("Customers: " + customer.Name + " " + customer.Surname + " " + customer.Age);
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("This customer deleted!: " + customer.Name + " " + customer.Surname);
+        }
+
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Customer customer1 = new Customer();
+            customer1.Name = "Şule";
+            customer1.Surname = "Kaptan";
+            customer1.Age = 21;
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Şevval";
+            customer2.Surname = "Arslan";
+            customer2.Age = 20;
+
+            Customer[] customers = new Customer[] { customer1, customer2 };
+
+
+            /*foreach (Customer cust in customers)
+            {
+                Console.WriteLine(cust.Name);
+                Console.WriteLine(cust.Surname);
+                Console.WriteLine(cust.Age);
+            }*/
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+
+            Console.WriteLine("-----------------------------------");
+
+            customerManager.List(customer1);
+            customerManager.List(customer2);
+
+            Console.WriteLine("-----------------------------------");
+
+            customerManager.Delete(customer2);
+
+
+        }
+    }
+}
